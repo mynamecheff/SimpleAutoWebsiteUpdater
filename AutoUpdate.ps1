7f296ddb68e104fffa22f801e04da21743402d31
mynamecheff <albertdepalo@gmail.com>
2023-04-26 13:05:31 +0200

    Update image

--- AutoUpdate.ps1
+++ AutoUpdate.ps1
@@ -1,45 +1,32 @@
 $websitePath = $PSScriptRoot
 $prevHash = ""
 
-# Prompt to enable autorun on startup
-$autorun = Read-Host "Do you want to enable autorun on startup? (y/n)"
-if ($autorun -eq "y") {
-  $autorunPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
-  $autorunName = "My Image Updater"
-  $autorunValue = "powershell -ExecutionPolicy Bypass -File `"$PSCommandPath`" autorun"
-  Set-ItemProperty $autorunPath $autorunName $autorunValue
-  Write-Host "Autorun enabled"
+function Check-Hash {
+  $hash = Get-FileHash -Algorithm MD5 -Path "$websitePath\image.png" | Select-Object -ExpandProperty Hash
+  if ($hash -eq $prevHash) {
+    return $false
+  }
+  $prevHash = $hash
+  return $true
 }
 
-if ($args[0] -eq "autorun") {
-  # Run in autorun mode
-  while ($true) {
-    $hash = Get-FileHash "image.png" -Algorithm SHA256 | Select-Object -ExpandProperty Hash
-    if ($hash -ne $prevHash) {
-      Copy-Item "image.png" $websitePath
-      cd $websitePath
-      git add .
-      git commit -m "Update image"
-      git push origin main
-      $prevHash = $hash
-    }
-    Start-Sleep -Seconds 600
-  }
-} elseif ($args[0] -eq "disable-autorun") {
-  # Disable autorun
-  $autorunPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
-  $autorunName = "My Image Updater"
-  Remove-ItemProperty $autorunPath $autorunName
-  Write-Host "Autorun disabled"
-} else {
-  # Run normally
-  $hash = Get-FileHash "image.png" -Algorithm SHA256 | Select-Object -ExpandProperty Hash
-  if ($hash -ne $prevHash) {
+# Prompt user to enable autorun on startup
+$answer = Read-Host "Do you want to enable autorun on startup? (y/n)"
+if ($answer -eq "y") {
+  $taskName = "Auto Update Website Image"
+  $trigger = New-ScheduledTaskTrigger -AtLogOn
+  $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass -File `"$PSScriptRoot\AutoUpdate.ps1`""
+  $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -RunOnlyIfIdle
+  Register-ScheduledTask -TaskName $taskName -Trigger $trigger -Action $action -Settings $settings
+}
+
+while ($true) {
+  if (Check-Hash) {
     Copy-Item "image.png" $websitePath
     cd $websitePath
     git add .
     git commit -m "Update image"
     git push origin main
-    $prevHash = $hash
   }
+  Start-Sleep -Seconds 600
 }
