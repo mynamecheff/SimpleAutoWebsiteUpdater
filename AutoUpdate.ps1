020b0b329c6d65e3f08320df8c68ecf5bdc3f062
mynamecheff <albertdepalo@gmail.com>
2023-04-26 13:30:53 +0200

    Update image

--- AutoUpdate.ps1
+++ AutoUpdate.ps1
@@ -1,41 +1,50 @@
-# Get the path of the script directory
-$scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Path
-
-# Prompt user to enable autorun on startup
-$autorun = Read-Host "Do you want to enable autorun on startup? (y/n)"
-if ($autorun -eq "y") {
-    $autorunPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
-    Set-ItemProperty -Path $autorunPath -Name "AutoUpdate" -Value $MyInvocation.MyCommand.Definition
+# Check if running as administrator
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+  Write-Host "Please run this script as an administrator." -ForegroundColor Red
+  Exit
 }
 
-# Initialize variables
-$tempImagePath = "$scriptPath\tempimage.png"
-$latestImageHash = ""
+# Prompt to enable autorun on startup
+$title = "Autorun on startup"
+$message = "Do you want to enable autorun on startup?"
+$yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", "Enables the script to run on startup."
+$no = New-Object System.Management.Automation.Host.ChoiceDescription "&No", "Disables autorun on startup."
+$options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no)
+$result = $Host.UI.PromptForChoice($title, $message, $options, 0)
+
+if ($result -eq 0) {
+  $taskName = "UpdateImage"
+  $taskPath = "$PSScriptRoot\UpdateImage.ps1"
+  $trigger = New-ScheduledTaskTrigger -AtStartup
+  $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-File `"$taskPath`""
+  $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -ExecutionTimeLimit (New-TimeSpan -Hours 1)
+  $task = New-ScheduledTask -Action $action -Trigger $trigger -Settings $settings
+  Register-ScheduledTask -TaskName $taskName -InputObject $task -Force
+  Write-Host "Autorun on startup has been enabled." -ForegroundColor Green
+} else {
+  Write-Host "Autorun on startup has been disabled." -ForegroundColor Yellow
+  Unregister-ScheduledTask -TaskName "UpdateImage" -Confirm:$false
+}
+
+# Verify if the image has changed before pushing to Git
+$imagePath = "$PSScriptRoot\image.png"
+$lastHash = ""
 
 while ($true) {
-    # Copy image to script directory
-    Copy-Item "$scriptPath\image.png" $tempImagePath -Force
+  $currentHash = Get-FileHash $imagePath -Algorithm SHA256 | Select-Object -ExpandProperty Hash
 
-    # Get hash of latest image
-    $latestImageHash = (Get-FileHash $tempImagePath).Hash
+  if ($currentHash -ne $lastHash) {
+      # Image has changed, push to Git
+      Copy-Item $imagePath $websitePath
+      Set-Location $websitePath
+      git add .
+      git commit -m "Update image"
+      git push origin main
+      Write-Host "Image has been updated and pushed to Git." -ForegroundColor Green
+      $lastHash = $currentHash
+  } else {
+      Write-Host "Image has not changed, no action taken." -ForegroundColor Yellow
+  }
 
-    # Compare hash of latest image with previous hash
-    if ($latestImageHash -ne $previousImageHash) {
-        # Copy latest image to website directory
-        Copy-Item $tempImagePath "$websitePath\image.png" -Force
-
-        # Change to website directory
-        Set-Location $websitePath
-
-        # Add, commit, and push changes
-        git add .
-        git commit -m "Update image"
-        git push origin main
-
-        # Update previous hash
-        $previousImageHash = $latestImageHash
-    }
-
-    # Wait 10 minutes before checking again
-    Start-Sleep -Seconds 600
+  Start-Sleep -Seconds 5
 }
