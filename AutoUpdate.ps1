144883a3bf1731916198ee85ab5b798d1c355340
mynamecheff <albertdepalo@gmail.com>
2023-04-26 13:09:39 +0200

    Update image

--- AutoUpdate.ps1
+++ AutoUpdate.ps1
@@ -1,32 +1,39 @@
+# Set the website path and image path
 $websitePath = $PSScriptRoot
-$prevHash = ""
+$imagePath = "$PSScriptRoot\image.png"
+$tempImagePath = "$PSScriptRoot\image1.png"
 
-function Check-Hash {
-  $hash = Get-FileHash -Algorithm MD5 -Path "$websitePath\image.png" | Select-Object -ExpandProperty Hash
-  if ($hash -eq $prevHash) {
-    return $false
-  }
-  $prevHash = $hash
-  return $true
-}
+# Prompt to enable autorun on startup
+do {
+    $autorun = Read-Host "Do you want to enable autorun on startup? (y/n): "
+} while ($autorun -notin @('y','n'))
 
-# Prompt user to enable autorun on startup
-$answer = Read-Host "Do you want to enable autorun on startup? (y/n)"
-if ($answer -eq "y") {
-  $taskName = "Auto Update Website Image"
-  $trigger = New-ScheduledTaskTrigger -AtLogOn
-  $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass -File `"$PSScriptRoot\AutoUpdate.ps1`""
-  $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -RunOnlyIfIdle
-  Register-ScheduledTask -TaskName $taskName -Trigger $trigger -Action $action -Settings $settings
+# Set autorun registry value
+if ($autorun -eq 'y') {
+    $regPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
+    $regName = "AutoUpdateWebsite"
+    $regValue = $PSCommandPath
+    Set-ItemProperty -Path $regPath -Name $regName -Value $regValue
 }
 
 while ($true) {
-  if (Check-Hash) {
-    Copy-Item "image.png" $websitePath
-    cd $websitePath
-    git add .
-    git commit -m "Update image"
-    git push origin main
-  }
-  Start-Sleep -Seconds 600
+    # Check if the image has changed
+    $imageHash = Get-FileHash $imagePath
+    $tempImageHash = Get-FileHash $tempImagePath
+    if ($imageHash.Hash -ne $tempImageHash.Hash) {
+        # Copy the image and commit changes
+        Copy-Item $imagePath $tempImagePath -Force
+        Remove-Item $imagePath
+        Rename-Item $tempImagePath -NewName "image.png"
+        cd $websitePath
+        git add .
+        git commit -m "Update image"
+        git push origin main
+    }
+
+    # Wait for 10 minutes
+    Start-Sleep -Seconds 600
 }
+
+# Close the console window
+Exit
